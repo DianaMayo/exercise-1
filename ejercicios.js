a273fbbc1083e05a9babf079e949c0bb30e142be
DianaMayo <dianamayo250208@gmail.com>
2023-04-11 13:11:44 -0400

    Agregando codigo del ejercicio

---
+++ ejercicios.js
@@ -0,0 +1,39 @@
+
+//countDown
+
+/*function countDown() {
+    var contador = 4;
+
+var timer = function () {
+    console.log(contador) ;
+    contador--;
+    if (contador === 0){
+    clearInterval(intervalo);
+    console.log("Done")
+}
+}; 
+var intervalo = setInterval(timer, 1000);
+}
+countDown()
+*/
+
+
+
+
+//randomGame
+function randomGame() {
+    var contador = 1;
+
+let diana = setInterval(funtion() {
+    number = Math.random();
+    time++
+    if(num >= .75) {
+        clearInterval(diana);
+        console.log("It took" + contador + "times.");
+    }
+};1000)
+}
+randomGame()
+
+
+        
