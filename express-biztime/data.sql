de414bd7975cfa2e12eba8304c0d26efda56fecd
DianaMayo <127867304+DianaMayo@users.noreply.github.com>
2024-01-18 11:21:50 -0500

    Add files via upload

--- express-biztime/data.sql
+++ express-biztime/data.sql
@@ -1,7 +1,3 @@
-DROP DATABASE IF EXISTS biztime;
-
-CREATE DATABASE biztime;
-
 \c biztime
 
 DROP TABLE IF EXISTS invoices;
--- express-biztime/db.js
+++ express-biztime/db.js
@@ -1,12 +1,2 @@
 /** Database setup for BizTime. */
 
-const { Client } = required("pg");
-
-const client = new Client({
-    connectionString: "postgresql:///biztime"
-  });
-  
-  client.connect();
-  
-  
-  module.exports = client;
